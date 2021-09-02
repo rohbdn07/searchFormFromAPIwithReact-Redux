668370d1fa467bda0b4a1bf782b5eca86429a11a
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-09-02 15:57:04 +0300

    form component with props and search query

--- src/components/searchForm/Form.js
+++ src/components/searchForm/Form.js
@@ -1,43 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getDataAction } from "../../redux/action/getData-action";
+import FormProps from "./FormProps";
 
 export default function Form() {
+    const dispatch = useDispatch();
+    const { pageCount } = useSelector((state) => state.dataReducer);
+    console.log("the page count is", pageCount);
+    const [inputField, setInputField] = useState({
+        search_for: "",
+    });
+
+    const [selectField, setSelectField] = useState("Author");
+
+    const inputsHandlerChange = (e) => {
+        setInputField({
+            search_for: e.target.value,
+        });
+    };
+
+    const selectedHandlerChange = (e) => {
+        setSelectField(e.target.value);
+    };
+
+    const booksPerPage = 20;
+    useEffect(() => {
+        submitHandler();
+    }, [pageCount]);
+
+    const queryParams = [
+        ["lookfor", `${inputField.search_for}`],
+        ["type", `${selectField}`],
+        ["field[]", "title"],
+        ["field[]", "authors"],
+        ["field[]", "cleanIsbn"],
+        ["field[]", "year"],
+        ["sort", "relevance,id asc"],
+        ["page", `${pageCount}`],
+        ["limit", `${booksPerPage}`],
+        ["prettyPrint", "false"],
+        ["lng", "fi"],
+    ];
+
+    const queryString = queryParams
+        .map(([key, value]) => `${key}=${value}`)
+        .join("&");
+
+    const submitHandler = async (e) => {
+        try {
+            const response = await fetch(
+                `https://api.finna.fi/api/v1/search?${queryString}`,
+                {
+                    headers: { Accept: "application/json" },
+                }
+            );
+            const data = await response.json();
+            dispatch(getDataAction(data));
+        } catch (error) {
+            console.log("there is an error", error);
+        }
+    };
     return (
         <>
-            <form className="d-block col-lg-5">
-                <div className="d-sm-block col-lg-4 card-body">
-                    <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">
-                            Author's name
-                        </label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                        />
-                        <div id="emailHelp" className="form-text">
-                            We'll see list of books
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <label
-                            for="exampleInputPassword1"
-                            className="form-label"
-                        >
-                            Book's title
-                        </label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="exampleInputPassword1"
-                        />
-                    </div>
-                    <button type="button" class="btn btn-outline-primary">
-                        Search
-                    </button>
-                </div>
-            </form>
-            <hr />
+            <FormProps
+                inputField={inputField}
+                inputsHandlerChange={inputsHandlerChange}
+                selectedHandlerChange={selectedHandlerChange}
+                submitHandler={submitHandler}
+                selectField={selectField}
+            />
         </>
     );
 }
