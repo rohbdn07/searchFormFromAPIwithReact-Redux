4913c0c43a14f3002ba7ed8754563a412d29ac28
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-09-08 23:35:36 +0300

    router-dom added to dom element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import NavBar from './components/navbar/NavBar'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import FormPage from './pages/formPage/FormPage'
 
 function App(): JSX.Element {
   return (
-    <div className="container">
-      <FormPage />
+    <div className="col-lg-12">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <NavBar />
+            <div className="container">
+              <FormPage />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
